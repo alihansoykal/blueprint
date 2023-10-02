6dc1fdd679223a19c70b279a74bf0c68b46ae7eb
Alihan Soykal <alihansoykal@gmail.com>
2023-10-03 02:16:28 +0300

    create themeModeProvider in providers

---
+++ lib/core/providers/providers.dart
@@ -0,0 +1,7 @@
+import 'package:blueprint/core/init/notifier/theme_mode_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final themeModeProvider = StateNotifierProvider<ThemeModeNotifier, ThemeMode>(
+  (ref) => ThemeModeNotifier(),
+);
