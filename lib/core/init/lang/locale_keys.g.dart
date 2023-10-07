21a04cd0b86101b3f6f1b44ba96643ad2b759951
Alihan Soykal <alihansoykal@gmail.com>
2023-10-07 23:27:48 +0300

    update locale keys

--- lib/core/init/lang/locale_keys.g.dart
+++ lib/core/init/lang/locale_keys.g.dart
@@ -5,9 +5,9 @@ abstract class  LocaleKeys {
   static const welcome_title = 'welcome.title';
   static const welcome_subtitle = 'welcome.subtitle';
   static const welcome_description = 'welcome.description';
-  static const welcome_test = 'welcome.test';
   static const welcome_login = 'welcome.login';
   static const welcome_register = 'welcome.register';
+  static const welcome_skip = 'welcome.skip';
   static const welcome = 'welcome';
 
 }
