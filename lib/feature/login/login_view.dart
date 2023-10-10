a08e210d181f20f95fce07d46883cd36103a9403
Alihan Soykal <alihansoykal@gmail.com>
2023-10-11 02:11:47 +0300

    create login view

--- lib/feature/login/login_view.dart
+++ lib/feature/login/login_view.dart
@@ -1,10 +1,23 @@
+import 'package:blueprint/core/extensions/context_extension.dart';
 import 'package:flutter/material.dart';
+
+import '_product/_widgets/layout/custom_painter.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        body: CustomPaint(
+          size: Size.infinite,
+          painter: CurvePainter(),
+          child: Container(
+            height: context.height / 2,
+          ),
+        ),
+      ),
+    );
   }
 }
