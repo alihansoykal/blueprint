c39170bc9ae7681068c41177d9ae8d7ddea432e7
Alihan Soykal <alihansoykal@gmail.com>
2023-11-12 05:26:44 +0300

    refactor login view

--- lib/feature/login/login_view.dart
+++ lib/feature/login/login_view.dart
@@ -2,19 +2,38 @@ import 'package:blueprint/core/extensions/context_extension.dart';
 import 'package:flutter/material.dart';
 
 import '_product/_widgets/layout/custom_painter.dart';
+import '_product/mixin/login_view_mixin.dart';
 
-class LoginView extends StatelessWidget {
-  const LoginView({super.key});
+final class LoginView extends StatelessWidget with LoginViewMixin {
+  LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: CustomPaint(
-          size: Size.infinite,
-          painter: CurvePainter(),
-          child: Container(
-            height: context.height / 2,
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: context.width,
+            height: context.height,
+            child: CustomPaint(
+              size: Size.infinite,
+              painter: LoginViewCurvePainter(),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                  verticalSpacer(context, 0.3),
+                  header(context),
+                  emailField,
+                  passwordField,
+                  forgotPassword(context),
+                  alternativeLoginButtons,
+                  verticalSpacer(context, 0.06),
+                  loginButton,
+                  dontHaveAnAccount(context),
+                  verticalSpacer(context, 0.04),
+                ]),
+              ),
+            ),
           ),
         ),
       ),
