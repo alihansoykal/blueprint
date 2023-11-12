0726ddac3ace8c802e90f905327f8bcba823c873
Alihan Soykal <alihansoykal@gmail.com>
2023-11-12 05:25:46 +0300

    add new icons to svgs

--- lib/app/resources/svgs.dart
+++ lib/app/resources/svgs.dart
@@ -4,4 +4,6 @@ class Svgs {
   Svgs._();
 
   static const String blueprintLogo = 'assets/vectors/blueprint_logo.svg';
+  static const String facebookIcon = 'assets/vectors/facebook_icon.svg';
+  static const String googleIcon = 'assets/vectors/google_icon.svg';
 }
