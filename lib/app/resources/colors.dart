2efd945689fc7eff799c486a0ee0407d92495e16
Alihan Soykal <alihansoykal@gmail.com>
2023-11-12 05:46:09 +0300

    add some colors

--- lib/app/resources/colors.dart
+++ lib/app/resources/colors.dart
@@ -9,4 +9,14 @@ class AppColors {
   static const tertiaryColor = Color(0xffE0F4FF);
   static const quaternaryColor = Color(0xffFFEED9);
   static const backgroundColor = Color(0xffEFF3F7);
+  static const contrastColor = Color(0xFFFF6B6B);
+
+  // TEXT COLORS
+  static const titleColor = Color(0xFF333333);
+  static const subtitleColor = Color(0xFF555555);
+  static const bodyTextColor = Color(0xFF666666);
+  static const linkColor = Color(0xFF1A73E8);
+  static const warningColor = Color(0xFFFFAA00);
+  static const successColor = Color(0xFF28A745);
+  static const errorColor = Color(0xFFDC3545);
 }
