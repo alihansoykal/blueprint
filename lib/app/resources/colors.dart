f923662a0e0c1f5bb3abde8a9b0697068b6543d6
Alihan Soykal <alihansoykal@gmail.com>
2023-11-12 05:25:35 +0300

    add app theme colors

---
+++ lib/app/resources/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  //APP THEME COLORS
+  static const primaryColor = Color(0xFF39A7FF);
+  static const secondaryColor = Color(0xFF87C4FF);
+  static const tertiaryColor = Color(0xffE0F4FF);
+  static const quaternaryColor = Color(0xffFFEED9);
+  static const backgroundColor = Color(0xffEFF3F7);
+}
